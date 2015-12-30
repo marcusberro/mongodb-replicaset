ef6987e04c431cbe3425053798cb6d14f031b95c
Marcus Berro <marcusberro@gmail.com>
2015-12-30 16:08:52 -0200

    Show message when trying to stop mongod processes

--- 01-simple-replset/purge-cluster.sh
+++ 01-simple-replset/purge-cluster.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-# Test if there is instances running
-kill $(ps aux | grep mongodb.conf | grep config | awk '{print $2}')
+# Stop replica set
+
+PIDS_MONGO=`ps aux | grep mongodb.conf | grep config | awk '{print $2}'`
+
+if [ -z "$PIDS_MONGO" ]; then
+	echo "Mongo replica set nodes are not running"
+else
+	echo "Stoping mongo instances"
+	for PID in $PIDS_MONGO; do
+		echo "Killing mongo process: $PID"
+		kill $PID
+	done 
+fi
 
 rm -rf rs1/db
 rm -rf rs2/db
--- 01-simple-replset/stop-cluster.sh
+++ 01-simple-replset/stop-cluster.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-# Test if there is instances running
-kill $(ps aux | grep mongodb.conf | grep config | awk '{print $2}')
+PIDS_MONGO=`ps aux | grep mongodb.conf | grep config | awk '{print $2}'`
+
+if [ -z "$PIDS_MONGO" ]; then
+	echo "Mongo replica set nodes are not running"
+else
+	echo "Stoping mongo instances"
+	for PID in $PIDS_MONGO; do
+		echo "Killing mongo process: $PID"
+		kill $PID
+	done 
+fi
